35582a595bb5db86669b7385a68f65634e64afab
iiinkston <1876364759@qq.com>
2024-12-20 18:27:08 +0800

    Updated app.js with new features

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
 
-app.use([authorize, logger])
+let { people } = require('./data')
+// const logger = require('./logger')
+// const authorize = require('./authorize')
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: false }))
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people })
+    // console.log(method, url, time)
+    // res.send('Home')
+})
 
-    console.log(method, url, time)
-    res.send('Home')
+app.post('/api/people', (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        return res.status(400).json({ sucess: false, msg: 'please provide name value' })
+    }
+    res.status(201).send('success')
+})
+
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please Provide Credentials')
 })
 
 app.get('/about', (req, res) => {
